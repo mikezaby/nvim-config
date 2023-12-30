dd13861a98f5cd2a2bce108dae8ccce99d6d62aa
Michalis Zamparas <mikezaby@gmail.com>
2023-12-30 18:27:59 +0200

    SHow visualy space and tabs

--- lua/mikezaby/set.lua
+++ lua/mikezaby/set.lua
@@ -3,3 +3,10 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+
+-- Set the characters for displaying tabs and trailing spaces
+vim.opt.listchars = { tab = '>-', space = '·' }
+-- Enable displaying the listchars
+vim.opt.list = true
+-- Disable line wrapping
+vim.opt.wrap = false
