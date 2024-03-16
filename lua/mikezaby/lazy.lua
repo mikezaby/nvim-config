94c5f28c1a909b62234e1069c45e89c2e9b4cc79
Michalis Zamparas <mikezaby@gmail.com>
2024-03-16 21:28:23 +0200

    Formating lazy.lua

--- lua/mikezaby/lazy.lua
+++ lua/mikezaby/lazy.lua
@@ -1,87 +1,89 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- UI
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  { 'nvim-treesitter/playground' },
+	-- UI
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "nvim-treesitter/playground" },
 
-  { "lewis6991/gitsigns.nvim", config = true },
-  {
-    'nvim-lualine/lualine.nvim',
-    config = true,
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
-  { 
-    'akinsho/bufferline.nvim', 
-    version = "*", 
-    dependencies = 'nvim-tree/nvim-web-devicons',
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-  },
-  { 'nacro90/numb.nvim', opts = { show_numbers = true } },
+	{ "lewis6991/gitsigns.nvim", config = true },
+	{
+		"nvim-lualine/lualine.nvim",
+		config = true,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+	},
+	{ "nacro90/numb.nvim", opts = { show_numbers = true } },
 
-  -- LSP
-  { 'VonHeikemen/lsp-zero.nvim', 
-    branch = 'v3.x', 
-    dependencies = {
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'}, 
-      {'neovim/nvim-lspconfig'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/nvim-cmp'},
-      {'L3MON4D3/LuaSnip'},
-    }
-  },
-  { 'hrsh7th/cmp-buffer' },
-  { 'hrsh7th/cmp-path' },
-  {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-  },
+	-- LSP
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/nvim-cmp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	},
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+	},
 
-  -- Utils
-  {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-  },
-  {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-    config = true
-  },
-  { 'mbbill/undotree' },
-  { "preservim/nerdcommenter" },
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-  },
-  {
-    'windwp/nvim-ts-autotag',
-    config = true
-  },
+	-- Utils
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
+	{ "mbbill/undotree" },
+	{ "preservim/nerdcommenter" },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = true,
+	},
 })
