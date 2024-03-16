b09935c53b7ff4d6c84ffbec84a1e3e13aeef51a
Michalis Zamparas <mikezaby@gmail.com>
2024-03-16 21:26:43 +0200

    Add rustfmt to conform

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,23 +1,24 @@
 local conform = require("conform")
 
 conform.setup({
-  formatters_by_ft = {
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    svelte = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
-    json = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
-    graphql = { "prettier" },
-    lua = { "stylua" },
-  },
-  format_on_save = {
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500,
-  },
+	formatters_by_ft = {
+		rust = { "rustfmt" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		svelte = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		graphql = { "prettier" },
+		lua = { "stylua" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	},
 })
