817ac20f19936413b00f84301870c9657e6a5f36
Michalis Zabaras <mikezaby@gmail.com>
2025-06-04 15:42:27 +0300

    Add truble and update

--- after/plugin/tsc.lua
+++ after/plugin/tsc.lua
@@ -2,6 +2,13 @@ require("tsc").setup({
 	auto_open_qflist = true,
 	auto_close_qflist = true,
 	auto_start_watch_mode = true,
+	use_trouble_qflist = true,
+	flags = {
+		noEmit = true,
+		project = function()
+			return utils.find_nearest_tsconfig()
+		end,
+	},
 
 	vim.api.nvim_create_autocmd("BufWritePost", {
 		pattern = { "*.ts", "*.tsx" },
