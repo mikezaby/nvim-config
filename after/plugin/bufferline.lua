03935cbcdcf7c14b0a9693f44ca2ad9b0bd98cf8
Michalis Zabaras <mikezaby@gmail.com>
2025-09-25 22:28:07 +0300

    Update bufferline config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -2,5 +2,4 @@ require("bufferline").setup({
 	options = {
 		mode = "tabs", -- set to "tabs" to only show tabpages instead
 	},
-	highlights = require("catppuccin.groups.integrations.bufferline").get(),
 })
