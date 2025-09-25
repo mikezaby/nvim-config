a078f52d0c2496cf1177831dc588f58cb736d4fd
Michalis Zabaras <mikezaby@gmail.com>
2025-09-25 21:56:53 +0300

    Re-add avante

--- lua/mikezaby/lazy.lua
+++ lua/mikezaby/lazy.lua
@@ -169,4 +169,25 @@ require("lazy").setup({
 			"nvim-treesitter/nvim-treesitter",
 		},
 	},
+	{
+		"yetone/avante.nvim",
+		config = true,
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 })
